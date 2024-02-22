4fd52fce100470cf6fcdd3c782fc42d36d165133
Sugene Mulpuri <sugenemulpuri9@gmail.com>
2024-02-21 20:17:43 -0600

    phase 1 javascript functions continued

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+saturdayFun();
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork();
+
+function wrapAdjective(string = "*") {
+    return function(parameter="star programmer") {
+        return `You are ${string}${parameter}${string}!`
+    }
+
+}
+
+wrapAdjective();
